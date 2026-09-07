985e56a04b2dfeded7c83b5e3e90bb8e3f7364d8
Hasini478 <hg4107@srmist.edu.in>
2026-09-07 10:54:39 +0530

    q3

---
+++ src/main/java/string/class_problems/BMICalculator.java
@@ -0,0 +1,35 @@
+package string.class_problems;
+
+public class BMICalculator {
+
+    static String getBmiStatus(double bmi) {
+        if (bmi < 18.5)
+            return "Underweight";
+        else if (bmi < 25)
+            return "Normal";
+        else if (bmi < 30)
+            return "Overweight";
+        else
+            return "Obese";
+    }
+
+    static void printWellnessReport(double[] heights, double[] weights) {
+        System.out.println("Person | Height | Weight | BMI | Status");
+
+        for (int i = 0; i < heights.length; i++) {
+            double bmi = weights[i] / (heights[i] * heights[i]);
+
+            System.out.printf("%d | %.2f | %.2f | %.2f | %s%n",
+                    i + 1, heights[i], weights[i],
+                    bmi, getBmiStatus(bmi));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        double[] heights = {1.75, 1.60, 1.70, 1.65, 1.80};
+        double[] weights = {70, 90, 65, 80, 85};
+
+        printWellnessReport(heights, weights);
+    }
+}
