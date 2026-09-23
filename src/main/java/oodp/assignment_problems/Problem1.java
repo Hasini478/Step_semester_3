7421875994d5fc278d15599e160d444ff777cc2f
Hasini478 <hg4107@srmist.edu.in>
2026-09-23 21:50:17 +0530

    q1

---
+++ src/main/java/oodp/assignment_problems/Problem1.java
@@ -0,0 +1,20 @@
+package oodp.assignment_problems;
+
+import java.util.Arrays;
+
+class Problem1 {
+
+    static void curveScores(int[] scores, int bonus) {
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = scores[i] + bonus;
+        }
+    }
+
+    static void main() {
+        int[] scores = {70, 85, 60};
+
+        curveScores(scores, 10);
+
+        System.out.println(Arrays.toString(scores));
+    }
+}
