14fc56a04e72d8ed77df8fac98414cca9a33e944
Hasini478 <hg4107@srmist.edu.in>
2026-09-23 22:35:48 +0530

    q3

---
+++ src/main/java/oopfundementals/assignment_problems/M3.java
@@ -0,0 +1,46 @@
+package oopfundementals.assignment_problems;
+
+class M3 {
+
+    static class PasswordChecker {
+
+        private final String password;
+
+        PasswordChecker(String password) {
+            this.password = password;
+        }
+
+        public String getStrength() {
+
+            int length = password.length();
+
+            if (length < 6) {
+                return "Weak";
+            }
+            else if (length <= 9) {
+                return "Medium";
+            }
+            else {
+                return "Strong";
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PasswordChecker pc1 =
+                new PasswordChecker("abcd");
+
+        System.out.println(pc1.getStrength());
+
+        PasswordChecker pc2 =
+                new PasswordChecker("abcdefghi");
+
+        System.out.println(pc2.getStrength());
+
+        PasswordChecker pc3 =
+                new PasswordChecker("abcdefghijkl");
+
+        System.out.println(pc3.getStrength());
+    }
+}
