95362ad6b33be2285ddd8423874e6e62defd3d1f
Hasini478 <hg4107@srmist.edu.in>
2026-09-23 22:36:35 +0530

    q5

---
+++ src/main/java/oopfundementals/assignment_problems/M5.java
@@ -0,0 +1,61 @@
+package oopfundementals.assignment_problems;
+
+class M5 {
+
+    static class Cart {
+
+        private double[] prices;
+        private int count;
+        private final String cartId;
+
+        Cart(String cartId, int size) {
+            this.cartId = cartId;
+            prices = new double[size];
+            count = 0;
+        }
+
+        public void addItem(double price) {
+
+            if (count < prices.length) {
+                prices[count] = price;
+                count++;
+            }
+        }
+
+        public double getTotal() {
+
+            double total = 0;
+
+            for (int i = 0; i < count; i++) {
+                total = total + prices[i];
+            }
+
+            return total;
+        }
+
+        public int getItemCount() {
+            return count;
+        }
+
+        public String getCartId() {
+            return cartId;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Cart cart = new Cart("CART-5", 20);
+
+        cart.addItem(250);
+        cart.addItem(99);
+        cart.addItem(151);
+
+        System.out.println(
+                "Total = " + cart.getTotal()
+        );
+
+        System.out.println(
+                "Item count = " + cart.getItemCount()
+        );
+    }
+}
